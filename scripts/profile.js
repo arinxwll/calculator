352a061fc99fbed1ed78a06b3c5ed9d83f361936
olkio <olkio8987@gmail.com>
2026-05-13 18:38:02 +0300

    add

--- scripts/profile.js
+++ scripts/profile.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
             memberSince: (JSON.parse(localStorage.getItem('userProfile')) || defaultUserProfile).memberSince
         };
 
-        
+
 
         localStorage.setItem('userProfile', JSON.stringify(updatedUserProfile));
         
@@ -91,3 +91,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
     updateProfileDisplay(); 
 });
+const express = require("express");
+const sql = require("mssql/msnodesqlv8");
+const path = require("path");
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const dbConfig = {
+  connectionString:
+    "Driver={ODBC Driver 17 for SQL Server};Server=A102PCPREPOD\\A102PCPREPOD;Database=students-Marina;Trusted_Connection=Yes;",
+  driver: "msnodesqlv8",
+};
+
+app.get("/students", async (req, res) => {
+  const connection = await sql.connect(dbConfig);
+
+  const result = await connection.request().query("SELECT * FROM dbo.students");
+
+  res.json(result.recordset);
+});
+
+app.post("/students", async (req, res) => {
+  const { name, lastname, birthday, group_id } = req.body;
+  const connection = await sql.connect(dbConfig);
+
+  await connection
+    .request()
+    .input("name", sql.VarChar, name)
+    .input("password", sql.Int, group_id).query(`
+      INSERT INTO dbo.students( name, lastname, birthday, group_id)
+      VALUES (@name, @lastname, @birthday, @group_id)
+    `);
+
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000!, http://localhost:3000");
+});
+
